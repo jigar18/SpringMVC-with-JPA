90769b1393c169b6aa642ba8b9fec9862da1d53d
Jigar <jigar.joshi0218@gmail.com>
2019-09-27 16:07:17 -0400

    files updated 2019-09-27C3

--- src/main/java/com/training/jpacode/controller/StudentController.java
+++ src/main/java/com/training/jpacode/controller/StudentController.java
@@ -1,59 +1,56 @@
-package com.training.jpacode.controller;
-
-import com.training.jpacode.domain.Student;
-import com.training.jpacode.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-
-@Controller
-public class StudentController {
-    @Autowired
-    private StudentService studentService;
-
-    @RequestMapping("/")
-    public String indexPage(Model model) {
-        List<Student> student = studentService.findAllStudents();
-        model.addAttribute("studentList", student);
-        return "index";
-    }
-
-    @RequestMapping("new")
-    public String newPage(Model model) {
-        Student student = new Student();
-        model.addAttribute("student", student);
-        return "new_student";
-    }
-
-    @RequestMapping("edit/{id}")
-    public ModelAndView editPage(@PathVariable int id) {
-        ModelAndView modelAndView = new ModelAndView("edit_student");
-        Student student = studentService.getStudentById(id);
-        modelAndView.addObject("student", student);
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(@ModelAttribute("student")Student student) {
-        studentService.saveStudent(student);
-        return "redirect:/";
-    }
-
-    @RequestMapping(value = "/cancel", method = RequestMethod.POST)
-    public String cancel() {
-        return "redirect:/";
-    }
-
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String delete(@PathVariable(name = "id") int id) {
-        studentService.deleteStudent(id);
-        return "redirect:/";
-    }
-}
+//package com.training.jpacode.controller;
+//
+//import com.training.jpacode.domain.Student;
+//import com.training.jpacode.service.StudentService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.List;
+//
+//@Controller
+//public class StudentController {
+//    @Autowired
+//    private StudentService studentService;
+//
+//    @RequestMapping("/")
+//    public String indexPage(Model model) {
+//        List<Student> student = studentService.findAllStudents();
+//        model.addAttribute("student", student);
+//        return "index";
+//    }
+//
+//    @RequestMapping("new")
+//    public String newPage(Model model) {
+//        Student student = new Student();
+//        model.addAttribute("student", student);
+//        return "new_student";
+//    }
+//
+//    @RequestMapping("edit/{id}")
+//    public ModelAndView editPage(@PathVariable int id) {
+//        ModelAndView modelAndView = new ModelAndView("edit_student");
+//        Student student = studentService.getStudentById(id);
+//        modelAndView.addObject("student", student);
+//        return modelAndView;
+//    }
+//
+//    @RequestMapping(value = "/save", method = RequestMethod.POST)
+//    public String save(@ModelAttribute("student")Student student) {
+//        studentService.saveStudent(student);
+//        return "redirect:/";
+//    }
+//
+//    @RequestMapping(value = "/cancel", method = RequestMethod.POST)
+//    public String cancel() {
+//        return "redirect:/";
+//    }
+//
+//    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+//    public String delete(@PathVariable(name = "id") int id) {
+//        studentService.deleteStudent(id);
+//        return "redirect:/";
+//    }
+//}
--- src/main/java/com/training/jpacode/controller/StudentRestController.java
+++ src/main/java/com/training/jpacode/controller/StudentRestController.java
@@ -1,4 +1,50 @@
 package com.training.jpacode.controller;
 
+import com.training.jpacode.domain.Student;
+import com.training.jpacode.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
 public class StudentRestController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    private List<Student> findStudents(@RequestParam(required = false) Integer id) {
+        if(id == null) {
+            return studentService.findAllStudents();
+        }else {
+            return Collections.singletonList(studentService.getStudentById(id));
+        }
+    }
+
+    @GetMapping("/{id}")
+    private Student findStudent(@PathVariable int id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public void enterNewStudent(@RequestBody Student student) {
+        studentService.saveStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public void updateStudent(@RequestBody Student student, @PathVariable Integer id) {
+        if(id == null) throw new RuntimeException("ID needed to update student");
+
+        Student currentStudent = studentService.getStudentById(id);
+        if(currentStudent == null) throw new IndexOutOfBoundsException("Student database not found");
+
+        currentStudent.setFirstName(student.getFirstName());
+        currentStudent.setLastName(student.getLastName());
+        currentStudent.setAge(student.getAge());
+
+        studentService.saveStudent(currentStudent);
+    }
 }
